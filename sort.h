4c9d602f2d3c19507112651fb3effd7aedea9bfc
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-12-17 23:00:41 +0200

    update to header file

--- sort.h
+++ sort.h
@@ -32,4 +32,11 @@ void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 
 void selection_sort(int *array, size_t size);
+
+void quick_sort(int *array, size_t size);
+
+int partition(int arr[], int low, int high);
+
+void quick_sort_helper(int *array, int low, int high, size_t size);
+
 #endif
