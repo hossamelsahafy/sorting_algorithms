eeb7bc95bc7761529bc65b0a17291a7353103758
hossamelsahafy <hossamelsa7afy54@gmail.com>
2023-12-17 20:23:30 +0200

    sorting by insertion method

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,29 +11,28 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *cur, *tmp;
+	listint_t *cur, *tmp;
 
-    if (list == NULL || *list == NULL)
-        return;
-
-    cur = (*list)->next;
-    while (cur != NULL)
-    {
-        while (cur->prev != NULL && cur->n < cur->prev->n)
-        {
-            tmp = cur;
-            if (cur->next != NULL)
-                cur->next->prev = tmp->prev;
-            cur->prev->next = tmp->next;
-            if (tmp->prev->prev == NULL)
-                *list = tmp;
-            else
-                tmp->prev->prev->next = tmp;
-            tmp->next = tmp->prev;
-            tmp->prev = tmp->prev->prev;
-            tmp->next->prev = tmp;
-            print_list((const listint_t *)*list);
-        }
-        cur = cur->next;
-    }
+	if (list == NULL || *list == NULL)
+		return;
+	cur = (*list)->next;
+	while (cur != NULL)
+	{
+		while (cur->prev != NULL && cur->n < cur->prev->n)
+		{
+			tmp = cur;
+			if (cur->next != NULL)
+				cur->next->prev = tmp->prev;
+			cur->prev->next = tmp->next;
+			if (tmp->prev->prev == NULL)
+				*list = tmp;
+			else
+				tmp->prev->prev->next = tmp;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+			print_list((const listint_t *)*list);
+		}
+		cur = cur->next;
+	}
 }
